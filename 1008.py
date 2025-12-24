3bca4fa41c66aca31918543c3ae069150f012ead
JannatJimu <jannatulfardaousjim@gmail.com>
2025-12-24 14:58:37 +0600

    added 1008 solution

---
+++ 1008.py
@@ -0,0 +1,6 @@
+NUMBER = int(input())
+WorkingHours = int(input())
+perHourAmount = float(input())
+Amount= WorkingHours*perHourAmount
+print(f"NUMBER = {NUMBER}")
+print(f"SALARY = U$ {Amount:.2f}")
